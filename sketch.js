04d2aa4f95cf87263d4ee3a8a35e0d4307d480ac
super-delicious <yixin.cao@sjsu.edu>
2020-09-28 22:10:53 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+Let dogs = ["dudu", "bobby", "milky", "olivia", "jackey", "tutu"];
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(200);
+
+ console.log("initial array is ")
+ console.log(dogs);
+
+ dogs.pop();
+  console.log("array after pop ")
+ console.log(dogs);
+
+ dogs.push("aya");
+ console.log("array after push ")
+console.log(dogs);
+
+
 }
 
+
+
+
+
 function draw() {
-  background(220);
+
+
+
 }
