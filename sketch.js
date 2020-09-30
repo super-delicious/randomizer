08da00a4ede7a0887cbcf71d0db8923981e3ad47
super-delicious <yixin.cao@sjsu.edu>
2020-09-29 18:24:46 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-Let dogs = [{
+let dogs = [{
   name: "dudu",
   color: "chocolate"
 }, {
@@ -16,56 +16,67 @@ Let dogs = [{
 }, {
   name: "tutu",
   color: "wave"
-}];
+}]
 
 let randomIndex;
-let counter = 0;
+
+let animating = false;
+
 
 function setup() {
   createCanvas(600, 600);
   background(200);
   textSize(32);
 
-  text("click to randomize", 50 ,50);
+  text("start", 50, 50);
 
-  setTimeout(changeBackground, 1000);
- // setInterval(changeBackground, 1000);
+  // setTimeout(changeBackground, 1000);
+  // setInterval(changeBackground, 1000);
 }
 
 
 function draw() {
 
-
-
-}
-
-
-function changeBackground(){
-  if (counter <= 5){
-    counter++;
-    console.log(counter)
-  background(random(255), random(255), random(255));
-  setTimeout(changeBackground, 1000);
-  }else {
-  background(random(255), random(5), random(5));
+  if (animating == true) {
+    fill(random(20, 0, 50));
+    curve(random(width), random(height), random(width), random(height), random(width), random(height), random(5, 50), random(50, 400));
   }
+
 }
 
 
+//function changeBackground(){
+// if (counter <= 5){
+//   counter++;
+//   console.log(counter)
+// background(random(255), random(255), random(255));
+// setTimeout(changeBackground, 1000);
+//  }else {
+//  background(random(255), random(5), random(5));
+// }
+//}
+
+function randomizer() {
+
+  animating = false;
+
+  if (dogs[0]) {
+    background(random(255, 255, 255));
+    randomIndex = int(random(dogs.length));
+    text(dogs[randomIndex].name, 50, 50);
+    dogs.splice(randomIndex, 2);
+  } else {
+    background(random(200, 255));
+    text("ends", 50, 50);
+  }
+
+
+
+}
 
 function mousePressed() {
+  animating = true;
+  setTimeout(randomizer, 3000);
 
-if (dogs[0]){
-  background(random(200,255));
-  randomIndex = int(random(dogs.length));
-  text(dogs{randomIndex}.name, 50, 50);
-  dogs.splice(ramdomIndex, 1);
-} else {
-  background(random(200, 255));
-  text("ends", 50, 50);
+
 }
-}
-
-
-
- 
