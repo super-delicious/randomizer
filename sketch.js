91d346833f36e0115fca6f0bb54bd30225b2281a
super-delicious <yixin.cao@sjsu.edu>
2020-09-29 17:40:39 -0700

    update

--- sketch.js
+++ sketch.js
@@ -19,10 +19,17 @@ Let dogs = [{
 }];
 
 let randomIndex;
+let counter = 0;
 
 function setup() {
   createCanvas(600, 600);
   background(200);
+  textSize(32);
+
+  text("click to randomize", 50 ,50);
+
+  setTimeout(changeBackground, 1000);
+ // setInterval(changeBackground, 1000);
 }
 
 
@@ -32,8 +39,33 @@ function draw() {
 
 }
 
+
+function changeBackground(){
+  if (counter <= 5){
+    counter++;
+    console.log(counter)
+  background(random(255), random(255), random(255));
+  setTimeout(changeBackground, 1000);
+  }else {
+  background(random(255), random(5), random(5));
+  }
+}
+
+
+
 function mousePressed() {
+
+if (dogs[0]){
   background(random(200,255));
   randomIndex = int(random(dogs.length));
   text(dogs{randomIndex}.name, 50, 50);
   dogs.splice(ramdomIndex, 1);
+} else {
+  background(random(200, 255));
+  text("ends", 50, 50);
+}
+}
+
+
+
+ 
